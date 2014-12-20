22dd49466ad76ff607058e36411d9a3995ad6741
Avadhut <avadhut.shelar@gmail.com>
2014-12-20 14:35:11 +0530

    R Prog Assign 2 commit 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,20 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-        inv<-NULL
-        set<-function(y){
-                x<<-y
+        inv<-NULL                       #inv variable to store inverse of matrix
+        
+        set<-function(y){               #set Function used to reset chache matrix with new values
+                x<<-y                   #<< assign a value to an object in an environment different from the current
                 inv<<-NULL
         }
-        get<-function(){x}
         
-        setinverse<-function(me){inv<<-me}
+        get<-function(){x}              #function to get the current values of matrix
         
-        getinverse<-function(){inv}
+        setinverse<-function(me){inv<<-me}      # function to set the newly calculated inverse
+        
+        getinverse<-function(){inv}             # function to get the present inverse value
                 
-        list(set=set,get=get,
+        list(set=set,get=get,              
              setinverse=setinverse,
-             getinverse=getinverse)
+             getinverse=getinverse)             #list is returned
 }
 
 
@@ -26,12 +28,17 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inv<-x$getinverse()
+        
         if(!is.null(inv)){
                 message("getting a cached copy")
                 return(inv)     #returning cached copy
         }
+        
         data<-x$get()           #getting original matrix
+        
         inv<-solve(data)        #calculate inverse first time
+        
         x$setinverse(inv)       #assign calculated inverse to makeCacheMatrix
+        
         inv                     #returning an inverted matrix
 }
